f93ac0d08f1d83438f74c03cca78bdb6f024535d
hiren0711 <hiprajapati2001@gmail.com>
2024-04-20 19:30:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
+// Lazily loaded components
+const Home = React.lazy(() => import("./pages/Home"));
+const Product = React.lazy(() => import("./pages/Product"));
+
+// Main App component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Set up BrowserRouter for handling routes
+    <BrowserRouter>
+      {/* Define routes using the Routes component */}
+      <Routes>
+        {/* Route for the Home page */}
+        <Route
+          index
+          path="/"
+          // Element to be rendered for the Home route
+          element={
+            <>
+              {/* Header component for the Home page */}
+              <Header />
+              {/* Container to center content vertically and horizontally */}
+              <div
+                className="container d-flex justify-content-center align-items-center"
+                style={{ minHeight: "85vh" }}
+              >
+                {/* Use React.Suspense for lazy loading the Home component */}
+                <Suspense
+                  fallback={
+                    // Fallback UI while Home component is being loaded
+                    <>
+                      <div class="text-center">
+                        <div class="spinner-border" role="status">
+                          <span class="visually-hidden">Loading...</span>
+                        </div>
+                      </div>
+                    </>
+                  }
+                >
+                  {/* Render the Home component */}
+                  <Home />
+                </Suspense>
+              </div>
+              {/* Footer component for the Home page */}
+              <Footer />
+            </>
+          }
+        ></Route>
+        {/* Route for the Product page */}
+        <Route
+          path="/product"
+          // Element to be rendered for the Product route
+          element={
+            <>
+              {/* Header component for the Product page */}
+              <Header />
+              {/* Container to center content vertically and horizontally */}
+              <div
+                className="container d-flex justify-content-center align-items-center"
+                style={{ minHeight: "85vh" }}
+              >
+                {/* Use React.Suspense for lazy loading the Product component */}
+                <Suspense
+                  fallback={
+                    // Fallback UI while Product component is being loaded
+                    <>
+                      <div class="text-center">
+                        <div class="spinner-border" role="status">
+                          <span class="visually-hidden">Loading...</span>
+                        </div>
+                      </div>
+                    </>
+                  }
+                >
+                  {/* Render the Product component */}
+                  <Product />
+                </Suspense>
+              </div>
+              {/* Footer component for the Product page */}
+              <Footer />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
